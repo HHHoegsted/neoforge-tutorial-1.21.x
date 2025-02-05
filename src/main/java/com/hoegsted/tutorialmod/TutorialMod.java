a7887436098476c870d3f3de70d4ecf327d7d84b
HHHoegsted <hh.hoegsted@gmail.com>
2025-02-05 14:37:34 +0100

    Custom items added

--- src/main/java/com/hoegsted/tutorialmod/TutorialMod.java
+++ src/main/java/com/hoegsted/tutorialmod/TutorialMod.java
@@ -1,5 +1,7 @@
 package com.hoegsted.tutorialmod;
 
+import com.hoegsted.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,10 +36,12 @@ public class TutorialMod
         modEventBus.addListener(this::commonSetup);
 
 
+
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -54,7 +58,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
